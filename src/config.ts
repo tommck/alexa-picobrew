ed5477837ca91f6c7ec3c361723dbc5c7aea5c95
Tom McKearney <thomas.mckearney@appliedis.com>
2017-01-02 15:36:05 -0500

    clearing out more private info

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    userId: "d9116b051f9243498fd637862d9a26fd",
-    appId: 'amzn1.ask.skill.0c15d277-0df0-45b3-8fc8-fa99730d8c03',
+    picobrewUserId: "YOUR_PICOBREW_USER_ID",
+    appId: 'YOUR_APP_ID',
     debug: false,
     testServer: false, // TODO: how to do this?
     auth: { // TODO: encrypt or something?
